a60e33617cc0bd468a472c6342155b4230d3a644
John Craigie <email": null>
2016-04-18 19:04:33 -0700

    Fix belongs to associations for heroku

--- app/models/movie_category.rb
+++ app/models/movie_category.rb
@@ -2,7 +2,8 @@ class MovieCategory < ActiveRecord::Base
   validates :movie, :category, presence: true
   validates :category, uniqueness: { scope: [:movie] }
 
-  belongs_to :movie, :category
+  belongs_to :movie
+  belongs_to :category
 
 
 end
