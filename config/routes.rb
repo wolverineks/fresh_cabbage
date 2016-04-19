35684ac27fe25dbbcd7330f0ce1d48bc6a627b81
John Craigie <email": null>
2016-04-19 09:58:05 -0700

    Add add movie show and index route with resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :movies, only: [:show, :index]
   # Recommended by Devise
   root to: "movies#index"
 
