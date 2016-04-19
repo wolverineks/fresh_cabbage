35e9c6293b7127ed3f64cde4494d08336a0e13ae
John Craigie <email": null>
2016-04-19 14:39:30 -0700

    Add correct syntax on movie index view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,17 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show]
 
+  def index
+    @reviews = Review.all.where("movie_id = #{params[:movie_id]}")
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
 end
