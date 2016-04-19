c884fb03327a71de932e52f42502cbea0457cc36
John Craigie <email": null>
2016-04-19 09:53:41 -0700

    Fixed MoviesController to set movie for the show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show]
   def index
   	# Number of categories to report on.
   	@NUM_CATS = 3
@@ -15,4 +16,21 @@ class MoviesController < ApplicationController
   	#	trending_movies << Movie.order()
   	#end
   end
+
+  def show
+    @reviews = @movie.reviews
+  end
+
+  private
+
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def movie_params
+      params.require(:movie).permit(:title, :mpaa_rating, :synopsis, :runtime, :release_date)
+    end
+
+
 end
