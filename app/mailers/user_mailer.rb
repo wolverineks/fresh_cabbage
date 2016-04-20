a4ce3ad7e6bf5310c65027492614d17ab70b6529
Jack L. Hamilton <jacklh@gmail.com>
2016-04-20 14:30:51 -0700

    remove hardcoded url in place of helper method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,8 @@ class UserMailer < ApplicationMailer
  
   def welcome_email(user)
     @user = user
-    @url  = 'https://quiet-harbor-47415.herokuapp.com/users/sign_in'
+    #@url  = 'https://quiet-harbor-47415.herokuapp.com/users/sign_in'
+    @url  = new_user_session_url
     email_with_name = %("#{@user.name}" <#{@user.email}>)
   	mail(to: email_with_name, subject: 'Welcome to Fresh Cabbage')
   	#mail(to: @user.email, subject: 'Welcome to My Awesome Site')
