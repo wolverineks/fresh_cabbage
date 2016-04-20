0fa0c117b871bd44480207877d32bfdcde6f2708
Mark Siemers <mark.siemers@gmail.com>
2016-04-20 11:27:15 -0700

    Add after commit hook for recalculating critic rating average

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ActiveRecord::Base
+  after_commit :recalculate_average_for_movie
+
   validates :value, :user, :movie, presence: true
   validates :user, uniqueness: { scope: [:movie] }
   belongs_to :user
@@ -13,5 +15,11 @@ class Rating < ActiveRecord::Base
 
   def self.average_user_rating
     user_ratings.average(:value)
+  end
+
+  private
+
+  def recalculate_average_for_movie
+    movie.reset_average_critic_rating
   end
 end
