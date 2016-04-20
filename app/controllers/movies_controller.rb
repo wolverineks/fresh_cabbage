7447aa35f906c56b3ba18d2325a1afd5e6787378
Xin Zhang <email": null>
2016-04-20 11:24:01 -0700

    add carousel images to index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,14 @@ class MoviesController < ApplicationController
   	# Number of movies / category
   	@NUM_MOVIES = 6
 
-  	@movies = Movie.limit(5)
+    @new_movies = Movie.new_releases.limit(15)
+    @top_movies = Movie.critically_acclaimed.limit(15)
+    top_drama = Category.find_by(name: 'Drama')
+    @top_drama = top_drama.movies.limit(15)
+    top_action = Category.find_by(name: 'Action')
+    @top_action = top_action.movies.limit(15)
+    top_comedy = Category.find_by(name: 'Comedy')
+    @top_comedy = top_comedy.movies.limit(15)
   	# Get random categories and populate with movies
   	#rand_cats = Category.order("RAND()").first(NUM_CATS)
   	#trending_movies = {}
