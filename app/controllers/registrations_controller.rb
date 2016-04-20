241bd6b98793bc80dc79f165378e0da833acb81e
Jack L. Hamilton <jacklh@gmail.com>
2016-04-20 12:07:47 -0700

    Move create from user to registration model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
 
+	def create
+	  @user = User.new(params[:user])
+	 
+	  respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+#        UserMailer.welcome_email(@user).deliver_later
+         UserMailer.welcome_email(@user).deliver_now
+ 
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+
   private
 
   def sign_up_params
