c373b47777d18cb9a5464d24f22787eef82550eb
wolv <wolverine.ks@gmail.com>
2016-04-20 12:22:28 -0700

    Add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :movies, only: [:show, :index] do
-    resources :reviews, only: [:show, :index]
     resources :ratings, only: [:create, :update]
+    resources :reviews, except: [:destroy]
   end
 
   resources :search, only: [:index]
