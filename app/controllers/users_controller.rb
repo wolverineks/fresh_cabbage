30be51aabcc35fd2d6cd34375f25bcd633c27d7d
Jack L. Hamilton <jacklh@gmail.com>
2016-04-20 12:02:38 -0700

    Move setup_mail to production.rb in config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
-	def create
-    @user = User.new(params[:user])
+# 	def create
+#     @user = User.new(params[:user])
  
-    respond_to do |format|
-      if @user.save
-        # Tell the UserMailer to send a welcome email after save
-#        UserMailer.welcome_email(@user).deliver_later
-         UserMailer.welcome_email(@user).deliver_now
+#     respond_to do |format|
+#       if @user.save
+#         # Tell the UserMailer to send a welcome email after save
+# #        UserMailer.welcome_email(@user).deliver_later
+#          UserMailer.welcome_email(@user).deliver_now
  
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#         format.html { redirect_to(@user, notice: 'User was successfully created.') }
+#         format.json { render json: @user, status: :created, location: @user }
+#       else
+#         format.html { render action: 'new' }
+#         format.json { render json: @user.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
 end
