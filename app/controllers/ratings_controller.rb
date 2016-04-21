2b4752f14bd1d9278b1d8c248fbcbf070b148be5
Mark Siemers <mark.siemers@gmail.com>
2016-04-21 00:16:00 -0700

    Refactored controller, using before filters and fancy rails responders

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,31 +1,33 @@
 class RatingsController < ApplicationController
+  before_action :authorize_user!, :get_movie, :get_rating, :set_rating_value
+  respond_to :html, :js
 
   def create
-    render nothing: true, status: 401 and return unless current_user
-
-    @movie = Movie.find(params[:movie_id])
-    @rating = Rating.find_or_initialize_by(user: current_user, movie: @movie)
-    @rating.value = params[:rating]
-
-    respond_to do |format|
-      @rating.save
-      format.html { redirect_to @movie }
-      format.js
-    end
+    flash[:notice] = 'Rating was successfully created.' if @rating.save
+    respond_with(@movie)
   end
 
   def update
-    redirect_to root_path unless current_user
+    flash[:notice] = 'Rating was successfully updated.' if @rating.save
+    respond_with(@movie)
+  end
 
-    @movie = Movie.find(params[:movie_id])
-    @rating = Rating.find_or_initialize_by(user: current_user, movie: @movie)
-    @rating.value = params[:rating]
-    @rating.save
+  private
 
-    respond_to do |format|
-      format.html { redirect_to @movie }
-      format.js
-    end
+  def authorize_user!
+    render nothing: true, status: 401 and return unless current_user
+  end
+
+  def get_movie
+    @movie ||= Movie.find(params[:movie_id])
+  end
+
+  def get_rating
+    @rating ||= Rating.find_or_initialize_by(user: current_user, movie: get_movie)
+  end
+
+  def set_rating_value
+    get_rating.value = params[:rating]
   end
 
 end
